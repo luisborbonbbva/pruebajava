1698721991770206cdf11d24fefc4b2b944cc748
luisborbonbbva <55005420+luisborbonbbva@users.noreply.github.com>
2019-09-08 21:55:59 -0500

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,17 @@
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        
+        String ret;
+        
+        if (number%2 == 0 ) {
+        
+        ret = "Even";
+        } else {
+        
+        ret = "Odd";
+        }
+        
+        return ret;
+        
+    }
+}
